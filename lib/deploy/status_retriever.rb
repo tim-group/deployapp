42dd2c3f60a9e9c95370b6402dd34feb9a047d71
Tomas Doran <tomas.doran@timgroup.com>
2013-01-15 19:14:23 +0000

    Try to pass a custom UA string when getting health and status pages

--- lib/deploy/status_retriever.rb
+++ lib/deploy/status_retriever.rb
@@ -20,7 +20,7 @@ class Deploy::StatusRetriever
   def get(url)
     uri = URI.parse(url)
     Net::HTTP.start(uri.host, uri.port) do |http|
-      http.get(uri.request_uri).body
+      http.get(uri.request_uri, {'User-Agent' => 'deploytool health  ↪retriever'}).body
     end
   end
 end
