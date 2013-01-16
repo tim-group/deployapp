95c975b1edc648a992d7cc116f3d9b9e8abebd7d
Tomas Doran <bobtfish@bobtfish.net>
2013-01-16 18:49:20 -0500

    Renamespace include_hash rspec matcher.
    Into the non-deprecated namespace, so it'll continue to work in
    upcoming rspec versions.

--- spec/support/matchers/include_hash.rb
+++ spec/support/matchers/include_hash.rb
@@ -1,13 +1,13 @@
-Spec::Matchers.define :include_hash do |expected|
+RSpec::Matchers.define :include_hash do |expected|
   match do |actual|
+    result = true
+    expected.each_pair { |key,value|
+      if actual[key]!=value
+        result = false
+      end
+    }
 
-  result = true
-  expected.each_pair {|key,value|
-    if actual[key]!=value
-      result = false
-    end
-  }
-
-  result
+    result
   end
 end
+
