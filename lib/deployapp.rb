2aed75ea8a26ac41524434e6cdba6c2a1004a457
Tom Denley <tom.denley@timgroup.com>
2014-11-14 14:50:34 +0000

    requirements for basic usage inlcude the composite resolver

--- lib/deployapp.rb
+++ lib/deployapp.rb
@@ -1,3 +1,3 @@
-require 'deployapp/artifact_resolvers/product_store_artifact_resolver'
+require 'deployapp/artifact_resolvers/composite_artifact_resolver'
 require 'deployapp/coord'
 require 'deployapp/util/config_file'
