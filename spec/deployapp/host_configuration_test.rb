20a2e725da9ff0ba6711dd612e061deac5987cd5
David Ellis <david.ellis@timgroup.com>
2015-02-06 08:55:08 +0000

    add cluster

---
+++ spec/deployapp/host_configuration_test.rb
@@ -0,0 +1,52 @@
+$LOAD_PATH << File.join(File.dirname(__FILE__), "..", "../lib")
+require 'test/unit'
+require 'fileutils'
+require 'deployapp/host_configuration'
+require 'deployapp/participation_service/memory'
+
+describe DeployApp::HostConfiguration do
+  it 'allows provides the default clustername per instance when none is specified' do
+    host_configuration = DeployApp::HostConfiguration.new
+    config = %[
+application_instance {
+    application "App"
+    group "blue"
+    type "none"
+}]
+    host_configuration.add(config)
+
+    status = host_configuration.status()
+
+    status.should eq(
+      [{:present=>false,
+        :group=>"blue",
+        :cluster=>"default",
+        :participating=>false,
+        :application=>"App",
+        :version=>nil,
+        :health=>nil}])
+  end
+
+  it 'allows us to configure clustername per instance' do
+    host_configuration = DeployApp::HostConfiguration.new
+    config = %[
+application_instance {
+    application "App"
+    group "blue"
+    cluster "A"
+    type "none"
+}]
+    host_configuration.add(config)
+
+    status = host_configuration.status()
+
+    status.should eq(
+      [{:present=>false,
+        :group=>"blue",
+        :cluster=>"A",
+        :participating=>false,
+        :application=>"App",
+        :version=>nil,
+        :health=>nil}])
+  end
+end
