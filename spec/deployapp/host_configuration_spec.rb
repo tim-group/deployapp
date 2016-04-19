b5f6d4194dccc9e02dfafd4e31db4f5e79d967aa
Graham Allan <graham.allan@timgroup.com>
2016-04-19 15:34:34 +0100

    Port a test case from test/unit to rspec.

--- spec/deployapp/host_configuration_spec.rb
+++ spec/deployapp/host_configuration_spec.rb
@@ -50,4 +50,25 @@ application_instance {
          :version => nil,
          :health => nil }])
   end
+
+  it 'builds an app instance' do
+    host_configuration = DeployApp::HostConfiguration.new
+    config = %(
+    application_instance {
+          application "App1"
+          group "blue"
+          additional_jvm_args "-Xms3m -Xmx5m"
+          type "none"
+    }
+    )
+    host_configuration.add(config)
+
+    host_configuration.application_instances.size.should be(1)
+    host_configuration.application_instances[0].application_instance_config.application.should eq('App1')
+    host_configuration.application_instances[0].application_instance_config.group.should eq('blue')
+    host_configuration.application_instances[0].application_instance_config.ssh_key_location.should eq('/root/.ssh/productstore')
+    host_configuration.application_instances[0].application_instance_config.home.should eq('/opt/apps/App1-blue')
+    host_configuration.application_instances[0].application_instance_config.config_filename.should eq('/opt/apps/App1-blue/config.properties')
+    host_configuration.application_instances[0].application_instance_config.run_as_user.should eq('app1')
+  end
 end
--- test/deployapp/host_configuration_test.rb
+++ test/deployapp/host_configuration_test.rb
@@ -5,36 +5,6 @@ require 'deployapp/host_configuration'
 require 'deployapp/participation_service/memory'
 
 class HostConfigurationTest < Test::Unit::TestCase
-  def test_config_definition_builds_app_instance
-    config = %(
-    application_instance {
-          application "App1"
-          group "blue"
-          additional_jvm_args "-Xms3m -Xmx5m"
-          type "none"
-    }
-        )
-
-    host_configuration = DeployApp::HostConfiguration.new
-    host_configuration.add(config)
-
-    assert_equal 1, host_configuration.application_instances.size
-    assert_equal "App1",
-                 host_configuration.application_instances[0].application_instance_config.application
-    assert_equal "blue",
-                 host_configuration.application_instances[0].application_instance_config.group
-    assert_equal "/root/.ssh/productstore",
-                 host_configuration.application_instances[0].application_instance_config.ssh_key_location
-
-    assert_equal "/opt/apps/App1-blue",
-                 host_configuration.application_instances[0].application_instance_config.home
-    assert_equal "/opt/apps/App1-blue/config.properties",
-                 host_configuration.application_instances[0].application_instance_config.config_filename
-
-    assert_equal "app1",
-                 host_configuration.application_instances[0].application_instance_config.run_as_user
-  end
-
   def test_loading_config_files_builds_many_instances
     for i in 0..4
       config = %(
