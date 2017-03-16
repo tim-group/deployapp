9bf720869dc424bddef5ff1aeb8259eeece57ec5
Richard Pearce <richard.pearce@timgroup.com>
2017-03-16 12:23:10 +0000

    rpearce: Make rspec 3.5 compliant

--- spec/deployapp/host_configuration_spec.rb
+++ spec/deployapp/host_configuration_spec.rb
@@ -16,7 +16,7 @@ describe DeployApp::HostConfiguration do
 
     status = host_configuration.status
 
-    status.should eq(
+    expect(status).to eql(
       [{ :present => false,
          :group => 'blue',
          :stoppable => false,
@@ -40,7 +40,7 @@ describe DeployApp::HostConfiguration do
 
     status = host_configuration.status
 
-    status.should eq(
+    expect(status).to eql(
       [{ :present => false,
          :group => 'blue',
          :stoppable => false,
@@ -63,13 +63,14 @@ describe DeployApp::HostConfiguration do
     )
     host_configuration.add(config)
 
-    host_configuration.application_instances.size.should be(1)
-    host_configuration.application_instances[0].application_instance_config.application.should eq('App1')
-    host_configuration.application_instances[0].application_instance_config.group.should eq('blue')
-    host_configuration.application_instances[0].application_instance_config.ssh_key_location.should eq('/root/.ssh/productstore')
-    host_configuration.application_instances[0].application_instance_config.home.should eq('/opt/apps/App1-blue')
-    host_configuration.application_instances[0].application_instance_config.config_filename.should eq('/opt/apps/App1-blue/config.properties')
-    host_configuration.application_instances[0].application_instance_config.run_as_user.should eq('app1')
+    expect(host_configuration.application_instances.size).to eql 1
+    app_instance_cfg = host_configuration.application_instances.first.application_instance_config
+    expect(app_instance_cfg.application).to eql 'App1'
+    expect(app_instance_cfg.group).to eql 'blue'
+    expect(app_instance_cfg.ssh_key_location).to eql '/root/.ssh/productstore'
+    expect(app_instance_cfg.home).to eql '/opt/apps/App1-blue'
+    expect(app_instance_cfg.config_filename).to eql '/opt/apps/App1-blue/config.properties'
+    expect(app_instance_cfg.run_as_user).to eql 'app1'
   end
 
   it 'builds many app instances if loading many config files' do
@@ -88,7 +89,7 @@ describe DeployApp::HostConfiguration do
 
     host_configuration = DeployApp::HostConfiguration.new
     host_configuration.parse('build/conf.d/')
-    host_configuration.application_instances.size.should eq(5)
+    expect(host_configuration.application_instances.size).to eql 5
   end
 
   it 'retrieves status for instance' do
@@ -104,12 +105,12 @@ describe DeployApp::HostConfiguration do
     end
 
     status = host_configuration.status
-    status.size.should eq(5)
-    status[4].should eq(:application => 'App4', :group => 'blue', :version => nil, :present => false,
+    expect(status.size).to eql 5
+    expect(status[4]).to eql(:application => 'App4', :group => 'blue', :version => nil, :present => false,
                         :participating => false, :health => nil, :cluster => 'default', :stoppable => false)
 
     app_status = host_configuration.status(:application => 'App4')
-    app_status.size.should eq(1)
+    expect(app_status.size).to eql 1
   end
 
   it 'identifies instances by key' do
@@ -125,7 +126,7 @@ describe DeployApp::HostConfiguration do
       host_configuration.add(config)
     end
     instance = host_configuration.get_application_instance(:application => 'App4', :group => 'blue')
-    instance.should_not be_nil
+    expect(instance).to_not be_nil
   end
 
   it 'raises error when no instance found by key' do
@@ -160,8 +161,8 @@ describe DeployApp::HostConfiguration do
 
     application_instance = host_configuration.application_instances[0]
 
-    application_instance.artifact_resolver.should_not be_nil
-    application_instance.application_communicator.should_not be_nil
+    expect(application_instance.artifact_resolver).to_not be_nil
+    expect(application_instance.application_communicator).to_not be_nil
   end
 
   it 'finds services in group' do
@@ -177,10 +178,10 @@ describe DeployApp::HostConfiguration do
     end
 
     status = host_configuration.status(:group => 'blue')
-    status.size.should eq(5)
+    expect(status.size).to eql 5
 
     status = host_configuration.status(:group => 'green')
-    status.size.should eq(0)
+    expect(status.size).to eql 0
   end
 
   it 'raises environment not found error when directory does not exist' do
