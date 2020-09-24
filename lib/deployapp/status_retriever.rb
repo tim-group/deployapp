844c6ed89dc29d1a6adda6b6a1b08d027416a4d8
Graham Carlyle <graham.carlyle@timgroup.com>
2020-09-24 09:11:38 +0100

    gcarlyle: add some logging for exception on http requests for app status

--- lib/deployapp/status_retriever.rb
+++ lib/deployapp/status_retriever.rb
@@ -1,17 +1,22 @@
 require 'deployapp/namespace'
 require 'deployapp/status'
+require 'deployapp/util/log'
 require 'net/http'
 
 include DeployApp
 
 class DeployApp::StatusRetriever
+  include DeployApp::Util::Log
+
   def retrieve(base_url)
     status = DeployApp::Status.new(true)
     %w(stoppable version health).each { |n|
       status.add(n, get("#{base_url}/info/#{n}"))
     }
     return status
-  rescue  Exception
+  rescue  Exception => e
+    logger.error("Problem retrieving status: #{e.inspect}")
+    logger.debug(e.backtrace)
     return DeployApp::Status.new(false)
   end
 
